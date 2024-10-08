5a40d0e5f225599a4b3c34d1983dab86c55cd04a
drahcirolever <100079431+drahcirolever@users.noreply.github.com>
2024-10-08 19:50:53 +0800

    Add files via upload

---
+++ php/Revelo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>PHP Fundamentals Activity</title>
+</head>
+
+<body>
+     <h2>PHP Fundamentals Activity</h2>
+     <h4>Please Select topic:</h4>
+
+     <button onclick="window.location.href='login.php'">Login</button>
+     <button onclick="window.location.href='quadratic.php'">Quadratic</button>
+     <button onclick="window.location.href='orders.php'">Menu</button>
+</body>
+
+</html>
